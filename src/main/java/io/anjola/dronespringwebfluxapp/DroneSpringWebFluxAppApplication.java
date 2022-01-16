7161369c02ab4bc302ee5e48aa24f928c085d60e
Sanni Lateef Anjola <sanni.anjola@gmail.com>
2022-01-16 15:14:15 +0100

    Initial project setup

---
+++ src/main/java/io/anjola/dronespringwebfluxapp/DroneSpringWebFluxAppApplication.java
@@ -0,0 +1,13 @@
+package io.anjola.dronespringwebfluxapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DroneSpringWebFluxAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DroneSpringWebFluxAppApplication.class, args);
+    }
+
+}
